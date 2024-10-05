c3aa7ea572e417cdfc370d14bcd2519d757ea3a9
JitendraTandia <Jitendratandia67@gmail.com>
2024-10-05 16:26:03 +0530

    added navbar comp. and category component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./component/NavBar";
+import HomePage from "./component/HomePage";
+import Categories from "./component/Categories";
 
 const App = () => {
   return (
-    
-      <BrowserRouter>
-        <Routes>
-          <Route path="/test" element={<NavBar/>}/>
-        </Routes>
-      </BrowserRouter>
-    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/test" element={<NavBar />} />
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/categories" element={<Categories/>} />
+        
+      </Routes>
+    </BrowserRouter>
   );
 };
 
---
+++ src/Data/NavItemData.js
@@ -0,0 +1,7 @@
+export const NavList = [
+  { title: "Home", url: "/" },
+  { title: "Shop", url: "/shop" },
+  { title: "About Us", url: "/about-us" },
+  { title: "Category", url: "/category" },
+ 
+];
