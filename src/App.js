9d05c5682fcc8a8896471fdbf9e2505fc96469d4
Luv Agrawal <la24092002@gmail.com>
2024-10-05 14:45:35 +0530

    Product page created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavBar from "./component/NavBar";
+import ProductPage from "./component/ProductPage";
 
 const App = () => {
   return (
     
       <BrowserRouter>
         <Routes>
-          <Route path="/test" element={<NavBar/>}/>
+          <Route path="/test" element={<ProductPage />}/>
         </Routes>
       </BrowserRouter>
     
