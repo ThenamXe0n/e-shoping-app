16c4f8ff0b22901bf45996b4ffc9ea98ffd0b6fa
TheNamXe0n <thenameet0@gmail.com>
2024-10-09 17:14:48 +0530

    "Updated ProductCard component styling and added price filtering functionality to ShopPage and setup data for json-server"

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,8 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
+import axios from "axios";
 
 const ShopPage = () => {
+  const [priceFilter,setPriceFilter] = useState(10000000000)
   const [productData, setProuductData] = useState([]);
+
+  async function loadProducts() {
+    try {
+      const res = await axios.get("http://localhost:8080/allproducts");
+      const data = res.data;
+      setProuductData(data);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  function handleFilter(){
+    let filteredProducts = productData.filter((item)=>item.finalprice<=priceFilter)
+    console.log("filtered",filteredProducts)
+    return filteredProducts
+  }
+
+
+  useEffect(() => {
+    loadProducts();
+  }, []);
 
   return (
     <div className="flex w-screen">
@@ -24,12 +47,12 @@ const ShopPage = () => {
         <div className="my-5 border-2  py-4 px-2 min-h-[60vh]   rounded-3xl ">
           {productData.length ? (
             <div className="grid gap-4 grid-cols-1 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
-              {productData.map((product) => (
+              {handleFilter().map((product) => (
                 <ProductCard
                   img={product?.thumbnail}
                   title={product?.title}
                   price={product.price}
-                  finalPrice={product.finalPrice}
+                  finalPrice={product.finalprice}
                   rating={product.rating}
                 />
               ))}
@@ -46,7 +69,13 @@ const ShopPage = () => {
       <section className="w-[15%] hidden lg:block md:block sm:hidden">
         <div id="filter-section" className="mt-24">
           <h3 className="text-center text-2xl font-bold">Filter By Price</h3>
-          <input className="w-full" type="range" max={1000} min={299} />
+          <input
+            className="w-full"
+            type="range"
+           onChange={(e)=>setPriceFilter(e.target.value)}
+            max={1000}
+            min={299}
+          />
           <div className="flex justify-between items-center">
             <span>$299</span>
             <span>$1000</span>
