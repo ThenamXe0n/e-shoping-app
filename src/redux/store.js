731e515aee8c0e45753efee2f49e336f00061834
yashrajmandloi30 <yashmandloi32@gmail.com>
2024-12-03 11:52:38 +0530

    initial Commit

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ProductReducer from "./product/productSlice"
-
-export const store = configureStore({reducer:{
-  product: ProductReducer,
-}});
-
-// store.dispatch(getProduct())
